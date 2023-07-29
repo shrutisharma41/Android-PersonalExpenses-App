d5ec83bd95ef86a7c7e2c5c6a30754b25f0a5f02
shrutisharma41 <140740010+shrutisharma41@users.noreply.github.com>
2023-07-30 04:02:26 +0530

    Add files via upload

---
+++ widgets/transaction_list.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import './transaction_item.dart';
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final Function deleteTx;
+  TransactionList(this.transactions, this.deleteTx);
+  @override
+  Widget build(BuildContext context) {
+    return transactions.isEmpty
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                Text(
+                  'No Transaction Added Yet!',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset(
+                    'assets/image/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            );
+          })
+        : ListView(
+            children: transactions
+                .map((tx) => TransactionItem(
+                      key: ValueKey(tx.id),
+                      transaction: tx,
+                      deleteTx: deleteTx,
+                    ))
+                .toList(),
+          );
+  }
+}
